6712e70227ee2ace777d6a6ffb195011b95e80c0
SHIJIN PS <98303341+shijinps@users.noreply.github.com>
2025-05-06 19:14:54 +0530

    Update urls.py

--- rentalcar/rentalcar/urls.py
+++ rentalcar/rentalcar/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
@@ -6,12 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('adminapp.url')),
+    path('', include('adminapp.urls')),  # Fixed typo from 'adminapp.url' to 'adminapp.urls'
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+# Serve static files during development (e.g., CSS, JavaScript, images)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-
-urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# Serve media files during development (e.g., user-uploaded images)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
